a140508336b27a6d210f1601ff53ad7c47169daf
unknown <kuba.lepik@seznam.cz>
2024-04-07 13:26:07 +0200

    purge updates

---
+++ run-purge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ---------------------------------------------------------------------
+# Bash script for removing unnessesary files
+# Written as part of an assignment during the BPC-SOS course at FEEC BUT
+# Authors: Jakub Lepik, Martin Moncek, Matej Baranyk
+#
+# This script is released under the GNU General Public License v3.0.
+# For more details, see: https://www.gnu.org/licenses/gpl-3.0.html
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+#                        Yum package removal
+# ---------------------------------------------------------------------
+
+echo -e "\e[1;33mRemoving unnessesary yum packages...\e[0m"
