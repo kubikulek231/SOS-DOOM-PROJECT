704983936af052c8b1afbd866f981544a02d7b3a
Jakub Lepik <32817965+kubikulek231@users.noreply.github.com>
2024-04-16 23:00:40 +0200

    making the headers consistent

--- get-current-os-size.sh
+++ get-current-os-size.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# ---------------------------------------------------------------------
+# Script for getting current OS size
+# Written as part of an assignment during the BPC-SOS course at FEEC BUT
+# Authors: Jakub Lepik, Martin Moncek, Matej Baranyk
+#
+# This file is released under the GNU General Public License v3.0.
+# For more details, see: https://www.gnu.org/licenses/gpl-3.0.html
+# ---------------------------------------------------------------------
 
 # Get current OS size
 du / --exclude=/{proc,sys,dev} -abc | sort -n | numfmt --to=iec-i --suffix=B --padding=7
